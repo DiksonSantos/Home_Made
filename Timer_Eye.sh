140ea5aecbcb6d666879be640cedaf60f3fb0772
DiksonSantos <Diksonnn@gmail.com>
2021-12-03 04:43:23 -0300

    Simplificação completa

--- Timer_Eye.sh
+++ Timer_Eye.sh
@@ -3,63 +3,37 @@ from datetime import datetime
 import time
 import subprocess
 
-MN = [str(x).zfill(2) for x in range(6, 12)]
-TD = [str(x).zfill(2) for x in range(12,18)]
-NT = [str(x).zfill(2) for x in range(18, 24)]
-MD = [str(x).zfill(2) for x in range(00, 6)]
 
+MD = [*range(0, 6)]
+MN = [*range(6, 13)]
+TD = [*range(13,18)]
+NT = [*range(18,24)]
 
-# PEGA HORA MINUTO E SEGUNDO ATUAL
-now = datetime.now()
-# APENAS A HORA (sem minutos e segundos)
-#current_hora = now.strftime("%H")
 
 while True:
-    time.sleep(3)
-    current_hora = now.strftime("%H")
+    time.sleep(30)
+    current_hora = datetime.now()
     # NOITE (EVENING)
-    if current_hora in NT:
+    if current_hora.hour in NT:
         time.sleep(5)
         subprocess.call('sct 3500', shell=True)
         
     
     # MADRUGADA & ATÉ 06 DA MANHÃ:
-    elif current_hora in MD:
+    elif current_hora.hour in MD:
         time.sleep(5)
         subprocess.call('sct 3200', shell=True)
         
     # MANHÃ -> 06 AO MEIO DIA:
-    elif current_hora in MN:
+    elif current_hora.hour in MN:
         time.sleep(1)
         #print('MANHÃ')
         subprocess.call('sct 4500', shell=True)
 
     # VESPERTINO
-    elif current_hora in TD:
+    elif current_hora.hour in TD:
         time.sleep(5)
         subprocess.call('sct 6300', shell=True)
 
-    
+
 # https://www.eizo.com/library/basics/color_temperature_on_an_LCD_monitor/
-
-
-
-#now = datetime.now()
-
-# MOSTRA HORA MIN & SEGUNDO ATUAL
-#current_time = now.strftime("%H:%M:%S")
-#print("Current Time =", current_time)
-
-# PEGA APENAS A HORA:
-#current_hora = now.strftime("%H")
-
-# CONVERTE EM UNIDADE 'INTEIRO'
-#convert = int(current_hora)
-
-#NT = [*range(18,24)]
-#TD = [*range(13,18)]
-#MD = [*range(0,7)]
-#MN = [*range(6, 13)]
-
-# COVNERTE A HORA PARA NUMERO INTEIRO
-#convert = int(current_hora)
